ea356cda2b8ff1cd533663b786f21dc8ea2955a0
Thomas Charlot <thomas.charlot@datasweet.fr>
2018-05-18 12:15:03 +0200

    Add build for 6.2.3, 6.2.4

--- build.sh
+++ build.sh
@@ -16,23 +16,25 @@ if [ -n "$1" ]; then
   build $1
 fi
 
-build 6.2.2
-build 6.2.1
-build 6.2.0
+build 6.2.4
+build 6.2.3
+# build 6.2.2
+# build 6.2.1
+# build 6.2.0
 
-build 6.1.3
-build 6.1.2
-build 6.1.1
-build 6.1.0
+# build 6.1.3
+# build 6.1.2
+# build 6.1.1
+# build 6.1.0
 
-build 6.0.1
-build 6.0.0
+# build 6.0.1
+# build 6.0.0
 
-build 5.6.7
-build 5.6.6
-build 5.6.5
-build 5.6.4
-build 5.6.3
-build 5.6.2
-build 5.6.1
-build 5.6.0
+# build 5.6.7
+# build 5.6.6
+# build 5.6.5
+# build 5.6.4
+# build 5.6.3
+# build 5.6.2
+# build 5.6.1
+# build 5.6.0
