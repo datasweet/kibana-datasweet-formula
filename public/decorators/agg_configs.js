bb3ec30265c019dfb960b9f5351e27206e8a4d4c
gustavomatheus <gustavo.matheus@revmob.com>
2018-08-07 10:14:38 -0300

    AggConfigs toDsl decorator created

---
+++ public/decorators/agg_configs.js
@@ -0,0 +1,28 @@
+import * as prov from 'ui/vis/agg_configs';
+
+export function decorateVisAggConfigsProvider(Private) {
+  const AggConfigs = prov.AggConfigs || Private(prov.VisAggConfigsProvider);
+
+  /**
+   * Recursively removes undefined values from object.
+   * @param {*} obj
+   */
+  function removeEmptyValues(obj) {
+    return Object.keys(obj)
+      .filter(k => obj[k] != null) // Filters out undefined and null objects
+      .reduce((newObj, k) =>
+        typeof obj[k] === 'object' ?
+          { ...newObj, [k]: removeEmptyValues(obj[k]) } : // Recursive call
+          { ...newObj, [k]: obj[k] },
+        {});
+  }
+
+  const toDslFn = AggConfigs.prototype.toDsl;
+  AggConfigs.prototype.toDsl = function () {
+    const isUsingFormula = !!this.vis.aggs.byTypeName.datasweet_formula;
+    const dsl = toDslFn.apply(this, arguments);
+    // Removes empty `datasweet_formula` aggs from dsl query if needed.
+    //  This happens when `vis.isHierarchical()` returns true
+    return isUsingFormula && this.vis.isHierarchical() ? removeEmptyValues(dsl) : dsl;
+  };
+};
