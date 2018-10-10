a54a6f2e1585f8186eb2297fcc90c34f061a45a7
gustavomatheus <gustavo.matheus@revmob.com>
2018-10-10 16:58:07 -0300

    ESLint code errors fixed

--- public/formula_hack.js
+++ public/formula_hack.js
@@ -16,45 +16,45 @@ const appId = chrome.getApp().id;
 if (appId === 'kibana' || appId === 'dashboardViewer') {
 
   uiModules
-  .get('datasweet/formula', ['kibana'])
-  .run((Private) => {
-    decorateVisAggConfigProvider(Private);
-    decorateVisAggConfigsProvider(Private);
-    decorateAggTypes(Private);
-    decorateTabbedAggResponseWriterProvider(Private);
-  });
+    .get('datasweet/formula', ['kibana'])
+    .run((Private) => {
+      decorateVisAggConfigProvider(Private);
+      decorateVisAggConfigsProvider(Private);
+      decorateAggTypes(Private);
+      decorateTabbedAggResponseWriterProvider(Private);
+    });
 
   uiModules
-  .get('app/visualize')
-  .config(($provide) => {
-    $provide.decorator('visEditorAggDirective', ($delegate) => {
-      // Original
-      const linkFn = $delegate[0].link;
-      const tpl = $delegate[0].template;
+    .get('app/visualize')
+    .config(($provide) => {
+      $provide.decorator('visEditorAggDirective', ($delegate) => {
+        // Original
+        const linkFn = $delegate[0].link;
+        const tpl = $delegate[0].template;
 
-      // Inject template
-      $delegate[0].template = tpl
-        .replace(/<!-- title -->[\s\S]*?<\/span>/m, titleTpl)
-        .replace('<!-- disable aggregation -->', hiddenTpl);
+        // Inject template
+        $delegate[0].template = tpl
+          .replace(/<!-- title -->[\s\S]*?<\/span>/m, titleTpl)
+          .replace('<!-- disable aggregation -->', hiddenTpl);
 
-      // Inject new function in scope
-      $delegate[0].compile = function (tElem, tAttr) {
-        return function newLinkFn($scope, $el, attrs, kbnForm) {
-          linkFn.apply($delegate[0], arguments);
+        // Inject new function in scope
+        $delegate[0].compile = function () {
+          return function newLinkFn($scope) {
+            linkFn.apply($delegate[0], arguments);
 
-          $scope.canHide = function (aggregation) {
-            const metricCount = reduce($scope.group, function (count, agg) {
-              return (agg.schema.name === aggregation.schema.name && !agg.hidden) ? ++count : count;
-            }, 0);
-            return metricCount > 1;
+            $scope.canHide = function (aggregation) {
+              const metricCount = reduce($scope.group, function (count, agg) {
+                return (agg.schema.name === aggregation.schema.name && !agg.hidden) ? ++count : count;
+              }, 0);
+              return metricCount > 1;
+            };
           };
         };
-      };
 
-      // get rid of the old link function since we return a link function in compile
-      delete $delegate[0].link;
+        // get rid of the old link function since we return a link function in compile
+        delete $delegate[0].link;
 
-      return $delegate;
+        return $delegate;
+      });
     });
-  });
 }
