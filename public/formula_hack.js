33576fb0a9469901f75ae598fd973c482dd08b24
Joey Andres <joey@jsonar.com>
2019-02-13 16:35:37 -0800

    SES: Test suite fix.

--- public/formula_hack.js
+++ public/formula_hack.js
@@ -9,52 +9,55 @@ import hiddenTpl from './decorators/agg_hidden.html';
 import titleTpl from './decorators/agg_title.html';
 import './decorators/agg_table';
 
+try {
+  const appId = chrome.getApp().id;
 
-const appId = chrome.getApp().id;
+  // Only inject decorator on kibana app
+  if (appId === 'kibana' || appId === 'dashboardViewer') {
 
-// Only inject decorator on kibana app
-if (appId === 'kibana' || appId === 'dashboardViewer') {
+    uiModules
+    .get('datasweet/formula', ['kibana'])
+    .run((Private) => {
+      decorateVisAggConfigProvider(Private);
+      decorateVisAggConfigsProvider(Private);
+      decorateAggTypes(Private);
+      decorateTabbedAggResponseWriterProvider(Private);
+    });
 
-  uiModules
-  .get('datasweet/formula', ['kibana'])
-  .run((Private) => {
-    decorateVisAggConfigProvider(Private);
-    decorateVisAggConfigsProvider(Private);
-    decorateAggTypes(Private);
-    decorateTabbedAggResponseWriterProvider(Private);
-  });
+    uiModules
+    .get('app/visualize')
+    .config(($provide) => {
+      $provide.decorator('visEditorAggDirective', ($delegate) => {
+        // Original
+        const linkFn = $delegate[0].link;
+        const tpl = $delegate[0].template;
 
-  uiModules
-  .get('app/visualize')
-  .config(($provide) => {
-    $provide.decorator('visEditorAggDirective', ($delegate) => {
-      // Original
-      const linkFn = $delegate[0].link;
-      const tpl = $delegate[0].template;
+        // Inject template
+        $delegate[0].template = tpl
+          .replace(/<!-- title -->[\s\S]*?<\/span>/m, titleTpl)
+          .replace('<!-- disable aggregation -->', hiddenTpl);
 
-      // Inject template
-      $delegate[0].template = tpl
-        .replace(/<!-- title -->[\s\S]*?<\/span>/m, titleTpl)
-        .replace('<!-- disable aggregation -->', hiddenTpl);
+        // Inject new function in scope
+        $delegate[0].compile = function (tElem, tAttr) {
+          return function newLinkFn($scope, $el, attrs, kbnForm) {
+            linkFn.apply($delegate[0], arguments);
 
-      // Inject new function in scope
-      $delegate[0].compile = function (tElem, tAttr) {
-        return function newLinkFn($scope, $el, attrs, kbnForm) {
-          linkFn.apply($delegate[0], arguments);
-
-          $scope.canHide = function (aggregation) {
-            const metricCount = reduce($scope.group, function (count, agg) {
-              return (agg.schema.name === aggregation.schema.name && !agg.hidden) ? ++count : count;
-            }, 0);
-            return metricCount > 1;
+            $scope.canHide = function (aggregation) {
+              const metricCount = reduce($scope.group, function (count, agg) {
+                return (agg.schema.name === aggregation.schema.name && !agg.hidden) ? ++count : count;
+              }, 0);
+              return metricCount > 1;
+            };
           };
         };
-      };
 
-      // get rid of the old link function since we return a link function in compile
-      delete $delegate[0].link;
+        // get rid of the old link function since we return a link function in compile
+        delete $delegate[0].link;
 
-      return $delegate;
+        return $delegate;
+      });
     });
-  });
+  }
+} catch (e) {
+  console.error(e);
 }
